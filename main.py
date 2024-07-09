844c04827104ccf19529609a3e7758975bb077d6
davidevanthomas3 <davidevanthomas3@gmail.com>
2024-07-09 21:16:47 +0000

    addign and refactoring modules to get basic team, game, and main modules workign and printing reports. will add in data fetching later to get fields.

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 from team import *
+from game import *
+
+teams = []
+games = []
+
+powerIndex = []
+rankings = []
+cfp_rankings = []
+top25 = []
+report_list = []
+
+def generate_top25():
+    pass
 
 def print_top25_reports():
     for team in top25:
         team.print_report()
 
 def print_other_reports():
-    for team in report_list:
+    for team in cfp_rankings or report_list:
         if team not in top25:
             team.print_report()
-
-teams = []
-top25 = []
-report_list = []
 
 def main():
     Team1 = Team("team1")
     teams.append(Team1)
-    top25.append(Team1)
+    generate_top25()
     report_list.append(Team1)
     #print_powerIndex()
-    #print_rankings()
+    #print_top25()
     #print_cfp_rankings()
     print_top25_reports()
     print_other_reports()
--- team.py
+++ team.py
@@ -1,27 +1,40 @@
-class Team():
+from game import *
+
+class Team:
     def __init__(self, name):
         self.name = name
-        self.powerIndex = 0
-        self.cfpRank = 0
-        self.rank = 0
+        self.conference = None
+        self.powerIndex = None
+        self.cfpRank = None
+        self.rank = None
         self.games_played = []
         self.record = [0,0,0]
         self.points = [0,0]
         self.yards = [0,0]
-        self.conference_champ = False
+        self.conf_champ = False
+        self.credits = 0
 
     def print_report(self):
         report_text = ""
         report_text += "__________________________________________________________\n"
         report_text += self.name + "\n"
-        report_text += "Power Index: " + str(self.powerIndex) + "\n"
-        report_text += "CFP Rank: " + str(self.cfpRank) + "\n"
-        report_text += "Rank: " + str(self.rank) + "\n"
+        if self.powerIndex != None:
+            report_text += "Power Index: " + str(self.powerIndex) + "\n"
+        if self.cfpRank != None:
+            report_text += "CFP Rank: " + str(self.cfpRank) + "\n"
+        if self.rank != None:
+            report_text += "Rank: " + str(self.rank) + "\n"
         report_text += "Record: " + str(self.record[0]) + "-" + str(self.record[1])
         if self.record[2] != 0:
             report_text += "-" + str(self.record[2])
         report_text += "\n"
         report_text += "Point Margin: " + str(self.points[0]-self.points[1]) + "\n"
         report_text += "Yard Margin: " + str(self.yards[0]-self.yards[1]) + "\n"
+        if self.conf_champ:
+            report_text += self.conference + "Champion\n"
+        report_text += "\n" + "Schedule:" + "\n"
+        for game in self.games_played:
+            report_text += game.home_team + " v " + game.away_team + " " + game.result + "\n"
+        report_text += "Credits: " + str(self.credits) + "\n"
         report_text += "__________________________________________________________\n"
         print(report_text)
