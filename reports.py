8da5e74fdfb7cf8245ab02c1871cfc767eeac621
davidevanthomas3 <davidevanthomas3@gmail.com>
2024-07-16 13:56:06 +0000

    added CFPRankings to report ouput

--- reports.py
+++ reports.py
@@ -6,6 +6,14 @@ def print_top25(top25):
     for index, team in enumerate(top25):
         print(f" {index+1}. {team.name} ({team.record[0]}-{team.record[1]}) ({team.credits})")
 
+def print_cfpRankings(cfpRankings):
+    if len(cfpRankings) == 0:
+        print("No CFP Rankings")
+        return
+    else:
+        for index, team in enumerate(cfpRankings):
+            print(f" {index+1}. {team.name}")
+
 def print_top25_reports(top25):
     for team in top25:
         print(team.report())
