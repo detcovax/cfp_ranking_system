3d121e2ff57b56e207566754dd435cee51624ede
davidevanthomas3 <davidevanthomas3@gmail.com>
2024-07-19 16:23:12 +0000

    outline of fetch proccesses

--- fetch.py
+++ fetch.py
@@ -1,1 +1,11 @@
 # This module will be used to connect to an online data source, fetch that data, and store in a format that can be used in the evaluation and generating steps.
+
+# Step 1: Define data source as a set of urls
+
+# Step 2: Request html from urls
+
+# Step 3: Parse html, identify and collect data
+
+# Step 4: Clean data and store in a json file to be accessed later
+
+# Step 5: Querry through data and prepare for evaluation
