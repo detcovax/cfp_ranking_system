9506ee2c32cbe248d10e261733a3078cfd9045c3
detcovax <davidevanthomas3@gmail.com>
2024-09-30 10:22:13 -0400

    updates to credits assignment and creation of a 'true rank' rankings!!!

--- evaluate.py
+++ evaluate.py
@@ -61,9 +61,10 @@ for team in team_list:
         home_team = game_info.get("home_team")
         away_team = game_info.get("away_team")
         opponent_name = home_team if home_team != team.name else away_team
-        opponent_power = power_index_rank.get(opponent_name, 0)  # Default to 0 if opponent not found
+        opponent_power = power_index_rank.get(opponent_name, len(power_index_rank))  # Default to last if opponent not found
         team_power = power_index_rank.get(team.name, 0)  # Get the current team's rank
-        power_diff = abs(team_power - opponent_power) # NEED TO FIX THE POWER DIFF
+        power_diff = team_power - opponent_power
+        
         if home_team == team.name:
             opponent_league = f'{game_info.get("away_division")}' + ' - ' + f'{game_info.get("away_conference")}'
         elif away_team == team.name:
@@ -72,64 +73,83 @@ for team in team_list:
             opponent_league = 'none'
 
         point_margin = abs(game_info.get("home_points")-game_info.get("away_points")) if game_info.get("home_points") is not None and game_info.get("away_points") is not None else 0
-        point_margin_multiplier = point_margin/7
+        
+        point_margin_multiplier = max(5,point_margin/7)
         if point_margin_multiplier < 1:
             point_margin_multiplier = 1
         
-        if game_info.get("result") == "Win":
+        # Base power multiplier
+        power_multiplier = 1
+
+        if game_info.get("result") == "Win":  
             win_scale = 1
             if "fbs" in opponent_league.lower():
-                win_scale *= 10
+                win_scale *= 5
                 if "big ten" in opponent_league.lower() or "sec" in opponent_league.lower():
-                    win_scale *= 5
+                    win_scale *= 10
                 elif "acc" in opponent_league.lower() or "big 12" in opponent_league.lower():
                     win_scale *= 3
             elif "fcs" in opponent_league.lower():
                 win_scale *= 2
             if opponent_power !=0:
                 if opponent_power < 5:
-                    win_scale *= 12
-                elif opponent_power < 10:
                     win_scale *= 10
-                elif opponent_power < 15:
-                    win_scale *= 7
-                elif opponent_power < 25:
+                elif opponent_power < 10:
                     win_scale *= 5
-                elif opponent_power < 50:
+                elif opponent_power < 15:
+                    win_scale *= 3.5
+                elif opponent_power < 25:
                     win_scale *= 3
-                elif opponent_power < 100:
+                elif opponent_power < 50:
                     win_scale *= 2
+                elif opponent_power < 100:
+                    win_scale *= 1.5
                     
         elif game_info.get("result") == "Loss" and point_margin > 0:
             win_scale = -1
             if "fbs" not in opponent_league.lower():
                 if "fcs" not in opponent_league.lower():
-                    win_scale *= 10
+                    win_scale *= 50
                 else:
-                    win_scale *= 5
+                    win_scale *= 20
             else:
                 if "big ten" not in opponent_league.lower() and "sec" not in opponent_league.lower():
                     if "acc" not in opponent_league.lower() and "big 12" not in opponent_league.lower():
-                        win_scale *= 3
+                        win_scale *= 10
                     else:
-                        win_scale *= 2
+                        win_scale *= 5
             if opponent_power < 100:
+                if opponent_power > 10:
+                    win_scale *= 1.5
                 if opponent_power > 25:
                     win_scale *= 2
                 elif opponent_power > 50:
-                    win_scale *= 3
+                    win_scale *= 5
             else:
-                win_scale *= 5
+                win_scale *= 10
                 
         else:
             win_scale = 0
         
-        credits = point_margin_multiplier*win_scale*power_diff/1000     # NEED TO FIX THE POWER DIFF (JUST MULTIPLIES BY ABS DIFF, NEED TO CONSIDER IF OPP IS HIGHER LOWER)
+        credits = win_scale*point_margin_multiplier*power_multiplier/10
         if abs(credits) < 1 and abs(credits) !=0 and point_margin > 0:
             credits = credits/abs(credits)
         credits = int(credits)
         team.games_played[game]["win_credits"] = credits
+        team.games_played[game]["win_scale"] = win_scale
+        team.games_played[game]["power_diff"] = power_diff
+        team.games_played[game]["power_diff_multiplier"] = power_multiplier
         team.credits += credits
+        
+    for game, game_info in team.remaining_games.items():
+        home_team = game_info.get("home_team")
+        away_team = game_info.get("away_team")
+        opponent_name = home_team if home_team != team.name else away_team
+        opponent_power = power_index_rank.get(opponent_name, 0)  # Default to 0 if opponent not found
+        team_power = power_index_rank.get(team.name, 0)  # Get the current team's rank
+        power_diff = team_power - opponent_power
+        team.remaining_games[game]["power_diff"] = power_diff
+        team.remaining_games[game]["power_diff_multiplier"] = power_multiplier
 
 rankings = sorted(power_index, key=lambda team: (-team.credits))
 
@@ -141,11 +161,31 @@ for rank, team in enumerate(rankings, start=1):
         opponent_name = home_team if home_team != team.name else away_team
         if game_info.get("result") == "Win" and rankings[rank-1].name == opponent_name and (rankings[rank-1].credits - team.credits) <= 100:
             rankings[rank], rankings[rank-1] = rankings[rank-1], rankings[rank]
+            
+rankings_rank_d = {team.name: rank for rank, team in enumerate(rankings, start=1)}
+
+# Define a SOS metric that is a measure of the combined rank/strength of the full schedule
+# Define a SOR metric that is a measure of the combined rank/strength of opponents played
+# Define a POR metric that is a measure of the combined power of opponents played
 
 for power, team in enumerate(power_index, start=1):
     team.power = power
 for rank, team in enumerate(rankings, start=1):
     team.rank = rank
+# for sos, team in enumerate(sos, start=1):
+#     team.sor = sos
+# for sor, team in enumerate(sor, start=1):
+#     team.sor = sor
+# for por, team in enumerate(por, start=1):
+#     team.sor = por
+
+# assign 'true rank'
+for team in rankings:
+    team.true_rank = team.power + team.rank
+true_rankings = sorted(rankings, key=lambda team: (team.true_rank, -team.credits, -team.margin))
+for true_rank, team in enumerate(true_rankings, start=1):
+    team.true_rank = true_rank
+true_rankings_rank_d = {team.name: true_rank for true_rank, team in enumerate(true_rankings, start=1)}
 
 with open('rankings.txt', 'w', encoding="utf-8") as output_file:
     output_file.write("Dave's Power Index:")
@@ -164,7 +204,7 @@ with open('rankings.txt', 'w', encoding="utf-8") as output_file:
             else:
                 output_file.write(f'(Error {team.record})')
             output_file.write(f' ({margin_sign}{team.margin})')
-    output_file.write('\n\n\nRankings:')
+    output_file.write('\n\n\nCredit Rankings:')
     for rank, team in enumerate(rankings, start=1):
         if rank > 30:
             break
@@ -179,16 +219,31 @@ with open('rankings.txt', 'w', encoding="utf-8") as output_file:
             else:
                 output_file.write(f'(Error {team.record})')
             output_file.write(f' ({team.credits})')
-            
+    output_file.write('\n\n\nTrue Rankings:')
+    for true_rank, team in enumerate(true_rankings, start=1):
+        if true_rank > 50:
+            break
+        else:
+            output_file.write('\n ')
+            output_file.write(f'{true_rank}. {team.name} ')
+            output_file.write("(C) " if team.champ == True else "")
+            if team.record[2] == 0:
+                output_file.write(f'({team.record[0]}-{team.record[1]})')
+            elif team.record[2] > 0:
+                output_file.write(f'({team.record[0]}-{team.record[1]}-{team.record[2]})')
+            else:
+                output_file.write(f'(Error {team.record})')
+            output_file.write(f' ({team.power + team.rank})')
             
 
 with open('reports.txt', 'w', encoding="utf-8") as output_file:
     output_file.write("Team Reports:\n")
-    for team in rankings:
+    for team in true_rankings:
         output_file.write(
             "\n"
-            f"{team.rank}. {team.name}" + (f"   {team.league} Champion" if team.champ==True else "") + "\n"
+            f"{team.true_rank}. {team.name}" + (f"   {team.league} Champion" if team.champ==True else "") + "\n"
             f"   Dave's Power Index Ranking: {team.power}\n"
+            f"   Credit Ranking: {team.rank}\n"
             f"   Record: {team.record[0]}-{team.record[1]}-{team.record[2]}\n"
             f"   Margin: {('+' if team.margin > 0 else '')}{team.margin}\n"
             f"   Total Credits: {team.credits}\n"
@@ -202,16 +257,21 @@ with open('reports.txt', 'w', encoding="utf-8") as output_file:
             away_points = game_info.get("away_points")
             notes = game_info.get("notes")
             credits = game_info.get("win_credits")
+            win_scale = game_info.get("win_scale")
+            power_diff = game_info.get("power_diff")
+            power_diff_multiplier = game_info.get("power_diff_multiplier")
             output_file.write(
-                f"      " + (f"{notes}: " if notes else "") + f"{home_team}" + f" ({home_points})" + " vs. " + f"{away_team}" + f" ({away_points})" + f" - {result} ({credits} Win Credits)" + "\n"
+                f"      " + (f"{notes}: " if notes else "") + f"{true_rankings_rank_d[home_team]}. {home_team}" + f" ({home_points})" + "   vs.   " + f"{true_rankings_rank_d[away_team]}. {away_team}" + f" ({away_points})" + f" - {result} ({credits} Win Credits)(scale: {win_scale})" + f" (Power Diff: {power_diff})" + "\n"
             )
+        output_file.write(f"    Games Remaining: {len(team.remaining_games)}\n")
         for game, game_info in team.remaining_games.items():
             week = game_info.get("week")
             home_team = game_info.get("home_team")
             away_team = game_info.get("away_team")
             notes = game_info.get("notes")
+            power_diff = game_info.get("power_diff")
+            power_diff_multiplier = game_info.get("power_diff_multiplier")
             output_file.write(
-                f"      Week {week} - " + (f"{notes}: " if notes else "") + f"{home_team}" + " vs. " + f"{away_team}" + "\n"
+                f"      Week {week} - " + (f"{notes}: " if notes else "") + f"{true_rankings_rank_d[home_team]}. {home_team}" + " vs. " + f"{true_rankings_rank_d[away_team]}. {away_team}" + f" (Power Diff: {power_diff})" + "\n"
             )
-        output_file.write(f"    Games Remaining: {len(team.remaining_games)}")
         output_file.write("\n")
